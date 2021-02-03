f1aa7e3b27b86edcd1f1ae0cfe6ab33c5fc60cde
Rolanda <46946118+rolandadifan@users.noreply.github.com>
2021-02-03 11:30:02 +0700

    first commit

---
+++ src/js/main.js
@@ -0,0 +1,18 @@
+new fullpage('#fullpage', {
+                //options here
+                autoScrolling: false,
+                scrollHorizontally: false
+            });
+
+            //methods
+            fullpage_api.setAllowScrolling(true);
+
+
+
+
+$('#myModal').on('shown.bs.modal', function () {
+  $('#myInput').trigger('focus')
+})
+
+
+  
